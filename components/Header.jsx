5d314d8bac27a68117a78492b5188de66c1b8244
bereket-7 <bekhonelign@gmail.com>
2025-01-18 04:24:02 +0300

    mobile and desktop foundation

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,9 +1,10 @@
 import Link from "next/link"
 import { Button } from "./ui/button"
+import { Nav } from "./Nav"
 const Header = () => {
   return (
     <header className="py-8 xl:py-12 text-white">
-        <div className="container mx-auto">
+        <div className="container mx-auto flex justify-between items-center">
             {/* {logo } */}
 
             <Link href="/">
@@ -11,8 +12,20 @@ const Header = () => {
                     Bereket<span className="text-accent">.</span>
                     </h1>
             </Link>
-            {/* {desktop nav} */}
-            <nav className="flex gap-8 items-center justify-end"></nav>
+
+
+            {/* {desktop nav and hire me button} */}
+
+            <div className="hidden xl:flex items-center gap-8">
+              <Nav />
+                <Link href="/contact">
+                  <Button>Hire me</Button>
+                </Link>
+            </div>
+            {/* {mobile nav} */}
+            <div className="xl:hidden">
+                mobile nav
+            </div>
         </div>
     </header>
   )
