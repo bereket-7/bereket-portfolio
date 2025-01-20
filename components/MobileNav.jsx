e06f50f7bec5a84878efe9d172262060cf69cc4f
bereket-7 <bekhonelign@gmail.com>
2025-01-20 21:04:37 +0300

    mobile nav

--- components/MobileNav.jsx
+++ components/MobileNav.jsx
@@ -1,7 +1,36 @@
+"use client"
 
+import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import { CiMenuFries } from 'react-icons/ci';
+
+const links = [
+  {
+    name: "home",
+    path: "/",
+  },
+  {
+    name: "services",
+    path: "/services",
+  },
+  {
+    name: "resume",
+    path: "/resume",
+  },
+  {
+    name: "work",
+    path: "/work",
+  },
+  {
+    name: "contact",
+    path: "/contact",
+  },
+
+];
 const MobileNav = () => {
   return (
-    <div>MobileNav</div>
+    <nav>MobileNav</nav>
   )
 };
 
