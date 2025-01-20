bde6228caa4b5dbbb248a3e0f18cfda9d2f4d814
bereket-7 <bekhonelign@gmail.com>
2025-01-20 04:41:42 +0300

    transition

--- components/PageTransition.jsx
+++ components/PageTransition.jsx
@@ -1,6 +1,21 @@
+"use client";
+import { usePathname } from "next/navigation";
+import { AnimatePresence, delay } from "framer-motion";
+
 const PageTransition = ({children}) => {
+    const pathname = usePathname();
   return (
     <AnimatePresence>
+        <div key={pathname}>
+            <motion.div 
+            initial={{ opacity: 1}}
+            animate={{ opacity: 0,
+                transition:{delay: 1, duration:0.4, ease: "easeInOut" },
+            }}
+
+           />
+
+        </div>
         {children}
     </AnimatePresence>
   )
