3b5d97b2013b0fddd2c754d6c37594a06afa946c
bereket-7 <bekhonelign@gmail.com>
2025-01-20 09:39:22 +0300

    stairs update

--- components/Stairs.jsx
+++ components/Stairs.jsx
@@ -1,7 +1,7 @@
 import { animate, motion } from "framer-motion";
 
 //variants
-const StairAnimation = {
+const stairAnimation = {
     initial: {
          top: "0%",
         },
@@ -18,10 +18,27 @@ const reverseIndex = (index) => {
     return totalSteps - index - 1;
 };
 const Stairs = () => {
-  return <>
-  </>;  (
-    <div>Stairs</div>
-  )
-}
+  return (
+  <>
+  {[...Array(6)].map((_, index) => {
+    return (
+        <motion.div 
+                    key={index}
+                    variants={stairAnimation}
+                    initial="initial"
+                    animate="animate"
+                    exit="exit"
+                    transition={{ 
+                        duration: 0.4,
+                        ease: "easeInOut", 
+                        delay: reverseIndex(index) * 0.1, 
+                    }}
+                    className="h-full w-full bg-white relative"
+                />
+        );
+    })}
+  </>
+);
+};
 
 export default Stairs;
