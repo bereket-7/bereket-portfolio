a84edb01541ece17d7f851004c9a8912e0353a97
bereket-7 <bekhonelign@gmail.com>
2025-01-23 02:00:50 +0300

    home page intro section

--- app/page.jsx
+++ app/page.jsx
@@ -1,6 +1,23 @@
-
-export default function Home() {
+import React from 'react';
+import { Button } from "@/components/ui/button";
+import { FiDownload } from "react-icons/fi";
+import { Section } from 'lucide-react';
+const Home = () => {
   return (
-    <main>Homepage</main>
+    <Section className="h-full">
+      <div className="container mx-auto h-full">
+          <div className="flex flex-col xl:flex-row items-center justify-between xl:pt-8 xl:pb-24">
+              {/* text */}
+              <div>
+                <span>Software Developer</span>
+                <h1>Hello I'm</h1>
+              </div>
+              {/* photo */}
+              <div> Photo </div>
+          </div>
+      </div>
+    </Section>
   );
-}
+};
+
+export default Home;
