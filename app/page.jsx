10f981f2374d5210c96f43f042a998ff10b56875
bereket-7 <bekhonelign@gmail.com>
2025-01-23 21:42:49 +0300

    fi download

--- app/page.jsx
+++ app/page.jsx
@@ -1,3 +1,4 @@
+import Social from "@/components/Social";
 import { Button } from "@/components/ui/button"
 import { FiDownload } from "react-icons/fi";
 const Home = () => {
@@ -16,13 +17,16 @@ const Home = () => {
                   for financial systems. including having experience on integration with third party apis.
                 </p>
                 {/* btn and socials */}
-                <div>
+                <div className="flex flex-col xl:flex-row items-center gap-8">
                 <Button variant="outline"
                 size="lg"
-                className="uppercase flex">
+                className="uppercase flex items-center gap-2">
                   <span>Download CV</span>
                   <FiDownload className="text-xl" /> 
                 </Button>
+                <div className="mb-8 xl:mb-0">
+                  <Social containerStyles="fle" />
+                </div>
                 </div>
               </div>
               {/* photo */}
