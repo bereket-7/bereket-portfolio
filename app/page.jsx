bd24376c4bc181d2e196d2fff7de419a72bff2d5
bereket-7 <bekhonelign@gmail.com>
2025-01-23 21:03:47 +0300

    slef intro sytle home page

--- app/page.jsx
+++ app/page.jsx
@@ -6,11 +6,24 @@ const Home = () => {
       <div className="container mx-auto h-full">
           <div className="flex flex-col xl:flex-row items-center justify-between xl:pt-8 xl:pb-24">
               {/* text */}
-              <div>
-                <span>Software Developer</span>
-                <h1 className="h1">
-                  Hello I'm <br /> <span>Bereket Honelign</span>
+              <div className="text-center xl:text-left">
+                <span className="text-xl">Software Developer</span>
+                <h1 className="h1 mb-6">
+                  Hello I'm <br /> <span className="text-accent">Bereket Honelign</span>
                 </h1>
+                <p className="max-w-[500px] mb-9 text-white/80">
+                  I excel at developing robust scalable secured systems , ranges from ecommerce websites to payment gateway development 
+                  for financial systems. including having experience on integration with third party apis.
+                </p>
+                {/* btn and socials */}
+                <div>
+                <Button variant="outline"
+                size="lg"
+                className="uppercase flex">
+                  <span>Download CV</span>
+                  <FiDownload className="text-xl" /> 
+                </Button>
+                </div>
               </div>
               {/* photo */}
               <div> Photo </div>
