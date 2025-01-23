398f588972610f6dfcdf31f841b6ef384215a47a
bereket-7 <bekhonelign@gmail.com>
2025-01-23 20:40:24 +0300

    home import fix

--- app/page.jsx
+++ app/page.jsx
@@ -1,22 +1,25 @@
-import { Button } from ".@/components/ui/button"
+import { Button } from "@/components/ui/button"
 import { FiDownload } from "react-icons/fi";
-import { Section } from 'lucide-react';
 const Home = () => {
   return (
-    <Section className="h-full">
+    <section className="h-full">
       <div className="container mx-auto h-full">
           <div className="flex flex-col xl:flex-row items-center justify-between xl:pt-8 xl:pb-24">
               {/* text */}
               <div>
                 <span>Software Developer</span>
-                <h1>Hello I'm <br /> <span>Bereket Honelign</span></h1>
+                <h1 className="h1">
+                  Hello I'm <br /> <span>Bereket Honelign</span>
+                </h1>
               </div>
               {/* photo */}
               <div> Photo </div>
           </div>
       </div>
-    </Section>
+    </section>
   );
 };
 
 export default Home;
+
+
